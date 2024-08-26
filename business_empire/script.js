cbe9565df8f36d1b99c6955fce6ed4ddfa4f565e
Lucas DANQUIGNY <lucas.dan.08022008@gmail.com>
2024-08-26 19:28:50 +0200

    26/08/2024

--- business_empire/script.js
+++ business_empire/script.js
@@ -1,79 +1,54 @@
-function calculateTime() {
-    circuit = document.getElementById('circuit').value;
-    voiture = document.getElementById('voiture').value;
-    temps = parseInt(document.getElementById('temps').value);
+function calculateConstruction() {
+    argent = document.getElementById('money').value;
 
-    circuits = {
-        "spa": spa,
-        "bahrein": bahrein,
-        "fuji": fuji,
-        "mans": mans,
-        "portimao": portimao,
-        "monza": monza,
-        "sebring": sebring
-    };
+    materials = {
+        "private_house": { "metal": 4, "builder": 4, "wood": 50, "concrete": 170 }, "office_building": { "metal": 12, "builder": 13, "wood": 400, "concrete": 1500 },
+        "trade_center": { "metal": 25, "builder": 90, "wood": 300, "concrete": 3800 }, "apartement_building": { "metal": 660, "builder": 150, "wood": 2200, "concrete": 30200 }, "skyscraper": { "metal": 1450, "builder": 800, "wood": 2800, "concrete": 190000 }
+    }
 
-    result = circuits[circuit](voiture, temps);
-    if (result != "Temps non disponible pour cette voiture") {
-        result = circuits[circuit](voiture, temps) + " L";
-    }
-    else {
-        result = circuits[circuit](voiture, temps)
-    }
-    document.getElementById('output').innerText = result;
-}
+    private_house = 0
+    office_building = 0
+    trade_center = 0
+    apartement_building = 0
+    skyscraper = 0
+    metal = 0
+    builder = 0
+    wood = 0
+    concrete = 0
 
-function spa(voiture, temps) {
-    if (voiture == "ferrari") {
-        return (Math.round((1.628 * temps + 7.2), 0)).toString();
-    }
-    if (voiture == "LMP2") {
-        return (Math.round((1.547 * temps + 6.9), 0)).toString();
-    }
-    return "Temps non disponible pour cette voiture";
-}
+    skyscraper = Math.floor(argent / 13655150);
+    argent -= skyscraper * 13655150
 
-function bahrein(voiture, temps) {
-    if (voiture == "ferrari") {
-        return (Math.round((1.384 * temps + 5.74), 0)).toString();
-    }
-    return "Temps non disponible pour cette voiture";
-}
+    apartement_building = Math.floor(argent / 3302560);
+    argent -= apartement_building * 3302560
 
-function fuji(voiture, temps) {
-    if (voiture == "ferrari") {
-        return (Math.round((1.224 * temps + 4.16), 0)).toString();
-    }
-    return "Temps non disponible pour cette voiture";
-}
+    trade_center = Math.floor(argent / 543575);
+    argent -= trade_center * 543575
 
-function mans(voiture, temps) {
-    if (voiture == "ferrari") {
-        return (Math.round((1.483 * temps + 12.76), 0)).toString();
-    }
-    if (voiture == "LMP2") {
-        return (Math.round((1.692 * temps + 12.92), 0)).toString();
-    }
-    return "Temps non disponible pour cette voiture";
-}
+    office_building = Math.floor(argent / 248602);
+    argent -= office_building * 248602
 
-function portimao(voiture, temps) {
-    if (voiture == "ferrari") {
-        return (Math.round((1.375 * temps + 4.98), 0)).toString();
-    }
-    return "Temps non disponible pour cette voiture";
-}
+    private_house = Math.floor(argent / 39474);
+    argent -= private_house * 39474
 
-function monza(voiture, temps) {
-    if (voiture == "ferrari") {
-        return (Math.round((1.484 * temps + 5.6), 0)).toString();
-    }
-    return "Temps non disponible pour cette voiture";
-}
+    construction_quantity = "private_house : {private_house} / office_building : {office_building} / trade_center : {trade_center} / apartement_building : {apartement_building} / skyscraper : {skyscraper}"
 
-function sebring(voiture, temps) {
-    if (voiture == "ferrari") {
-        return (Math.round((1.424 * temps + 5.98), 0)).toString();
+    quantity = {
+        "private_house": int(private_house), "office_building": int(office_building), "trade_center": int(
+            trade_center), "apartement_building": int(apartement_building), "skyscraper": int(skyscraper),
     }
-    return "Temps non disponible pour cette voiture";
+
+    for (i in quantity) {
+        for (a in range(quantity[i])) {
+            metal += materials[i]["metal"]
+            builder += materials[i]["builder"]
+            wood += materials[i]["wood"]
+            concrete += materials[i]["concrete"]
+        }
+    }
+
+    construction_material = "metal : " + metal + "/ builder : " + builder + " / wood : " + wood + "/ concrete : " + concrete
+
+    document.getElementById('quantity').innerText = construction_quantity;
+    document.getElementById('materials').innerText = construction_material;
 }
